a41a4f5e9fae787365eebace0a28f8b53095f24c
VangThao1905 <vangthao1905@gmail.com>
2023-05-18 14:37:46 +0700

    implement todos app

---
+++ bloc_weather/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:bloc_weather/app.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:weather_repository/weather_repository.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: kIsWeb
+          ? HydratedStorage.webStorageDirectory
+          : await getTemporaryDirectory());
+  runApp(WeatherApp(weatherRepository: WeatherRepository()));
+}
