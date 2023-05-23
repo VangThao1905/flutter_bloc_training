7034b85c3dda420f7d2bbb6aaaa281e74802bf7e
VangThao1905 <vangthao1905@gmail.com>
2023-05-23 18:37:26 +0700

    implement todos_api

--- flutter_todos/packages/todos_api/lib/src/todos_api.dart
+++ flutter_todos/packages/todos_api/lib/src/todos_api.dart
@@ -1,7 +1,14 @@
+import 'models/models.dart';
+
 /// {@template todos_api}
 /// The interface and models for an API providing access to todos.
 /// {@endtemplate}
-class TodosApi {
+ class TodosApi {
   /// {@macro todos_api}
   const TodosApi();
+
+  Stream<List<Todo>> getTodos() {
+    // TODO: implement getTodos
+    throw UnimplementedError();
+  }
 }
